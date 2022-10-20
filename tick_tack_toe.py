cc27cd3b41e8df4222c5aa0ffb0e7950daa253ab
Siggywiggy <kurmaadam@gmail.com>
2022-10-20 22:49:58 +0300

    Added match logic

--- tick_tack_toe.py
+++ tick_tack_toe.py
@@ -1,5 +1,5 @@
 class Gameboard:
-    def __init__(self, num_of_matches=1):
+    def __init__(self):
 
         self.name = "Gameboard"
         self.board_rows = {
@@ -7,16 +7,16 @@ class Gameboard:
             2: ["_", "_", "_"],
             3: ["_", "_", "_"],
         }
-        self.rounds = num_of_matches
+        self.rounds = 1
 
     def __repr__(self):
 
-        return "{p1_name} and {p2_name}, lets play a game of Tick-tack-toe for {rounds} rounds! \n{p1_name}, your mark will be [{p1_icon}] and {p2_name}, yours will be [{p2_icon}]".format(
-            p1_name=player_1.player_name,
-            p2_name=player_2.player_name,
+        return "{p1_name} and {p2_name}, lets play a game of Tick-tack-toe for {rounds} more rounds! \n{p1_name}, your mark will be [{p1_icon}] and {p2_name}, yours will be [{p2_icon}]".format(
+            p1_name=player1.player_name,
+            p2_name=player2.player_name,
             rounds=self.rounds,
-            p1_icon=player_1.player_icon,
-            p2_icon=player_2.player_icon,
+            p1_icon=player1.player_icon,
+            p2_icon=player2.player_icon,
         )
 
     def draw_board(self):
@@ -31,6 +31,19 @@ class Gameboard:
         print(bottom_bar)
         return
 
+    def match_ended(self):
+        if player1.determine_winning() is True or player2.determine_winning() is True:
+            return True
+        else:
+            return False
+
+    def board_full(self):
+        if "_" not in self.board_rows.values():
+            print("No winner this round!")
+            return True
+        else:
+            return False
+
 
 class Player:
     def __init__(self, player_name, player_icon="x"):
@@ -38,8 +51,9 @@ class Player:
         self.player_score = 0
         self.player_icon = player_icon
         self.player_moves = []
-        # self.column_choice = 0
-        # self.row_choice = 0
+
+    def __repr__(self):
+        return f"Player {self.player_name}"
 
     def prompt_move(self):
 
@@ -84,116 +98,125 @@ class Player:
             print("Your column choice is out of 1-2-3 range! Choose your move again!")
             self.prompt_move()
 
-        elif self.data_input_row_choice not in new_board.board_rows.keys():
+        elif self.data_input_row_choice not in game.board_rows.keys():
             print("Your row choice is out of 1-2-3 range! Choose your move again!")
             self.prompt_move()
 
         elif (
-            new_board.board_rows[self.data_input_row_choice][
-                self.data_input_column_choice
-            ]
+            game.board_rows[self.data_input_row_choice][self.data_input_column_choice]
             != "_"
         ):
             print("There is already a mark there!")
             self.prompt_move()
         else:
-            new_board.board_rows[self.data_input_row_choice][
+            game.board_rows[self.data_input_row_choice][
                 self.data_input_column_choice
             ] = self.player_icon
-            player_moves.append([self.data_input_row_choice -1, self.data_input_column_choice])
+            self.player_moves.append(
+                [self.data_input_row_choice - 1, self.data_input_column_choice]
+            )
 
     def determine_winning(self):
-    
+
         # credit for logic https://jayeshkawli.ghost.io/tic-tac-toe/
-        player_moves_list = self.player_moves
         player_won = False
 
-        def check_win_rows(self):
-        
-            for row_num in range(0,3): #row = 0, row = 1, row = 2
-                 # Adding all player moves with 0 index = row to a list
-                row_container = [1 for player_move in player_moves_list if player_move[0] == row_num]
-                # Checking if player has made 3 moves in one row and returning True if so - player won
-                if sum(row_container) == 3:
-                    print(f'{self.player_name} won by row!')
-                    return player_won = True   
-                else:
-                    continue
-    
-        def check_win_columns(self):
+        # Checking if player has made 3 moves in one row and returning True if so - player won
 
-            for column_num in range(0,3): #column = 0, column = 1, column = 2
-                # Adding all player moves with 1 index = column to a list
-                column_container = [1 for player_move in player_moves_list if player_move[1] == column_num]
-                # Checking if player has made 3 moves in one column and returning True if so - player won
-                if sum(column_container) == 3:
-                    print(f'{self.player_name} won by column!')
-                    return player_won = True    
-                else:
-                    continue
-    
-        def check_digonal(self):
+        for row_num in range(0, 3):  # row = 0, row = 1, row = 2
+            row_container = [1 for move in self.player_moves if move[0] == row_num]
+            # Checking if player has made 3 moves in one row and returning True if so - player won
+            if sum(row_container) == 3:
+                print(f"{self.player_name} won by row!")
+                player_won = True
 
-            # Adding a "1" to a list if a player move had equal x and y "coordinates" - moves 1:1, 2:2, 3:3
-            diagonal_container = [1 for player_move in player_moves_list if player_move[0] == player_move[1]]
+        # Checking if player has made 3 moves in one column and returning True if so - player won
 
-            if sum(diagonal_container) == 3:
-                print(f'{self.player_name} won by diagonal!')
-                return player_won = True  
-            else:
-                continue
-    
-        def check_opposite_diagonal(self):
-            #if moves are 0:2, 1,1 and 2,0 - opposite diagonal, then x + y + 1 = 3 so filtering these moves matching this condition out to a container 
-            opposite_diagonal_container = [1 for player_move in player_move_list if player_move[0] + player_move[1] + 1 == 3]
+        for column_num in range(0, 3):  # column = 0, column = 1, column = 2
+            column_container = [
+                1 for move in self.player_moves if move[1] == column_num
+            ]
 
-            if sum(opposite_diagonal_container) == 3:
-                print(f'{self.player_name} won by opposite diagonal!')
-                return player_won = True 
-            else:
-                continue
-        
-        check_win_rows(self.player_moves_list)
-        check_win_columns(self.player_moves_list)
-        check_digonal(self.player_moves_list):
-        check_opposite_diagonal(self.player_moves_list)
+            if sum(column_container) == 3:
+                print(f"{self.player_name} won by column!")
+                player_won = True
 
+        # Container for moves that are 1,1, 2,2, 3,3
+
+        diagonal_container = [1 for move in self.player_moves if move[0] == move[1]]
+
+        if sum(diagonal_container) == 3:
+            print(f"{self.player_name} won by diagonal!")
+            player_won = True
+
+        # if moves are 0:2, 1,1 and 2,0 - opposite diagonal, then x + y + 1 = 3 so filtering these moves matching this condition out to a container
+        opposite_diagonal_container = [
+            1 for move in self.player_moves if move[0] + move[1] + 1 == 3
+        ]
+
+        if sum(opposite_diagonal_container) == 3:
+            print(f"{self.player_name} won by opposite diagonal!")
+            player_won = True
+
+        # Returning if the player instance has met winning condition
         return player_won
 
-
-
-
+    def add_score(self):
+        self.player_score += 1
 
 
 # Entering a number of matches the players want to play and checking if the match count is a valid number
+def get_num_matches():
+    num_of_matches = input(
+        "Welcome to a game of Tick-Tack-Toe, choose how many rounds you want to play: "
+    )
 
-num_of_matches = input(
-    "Welcome to a game of Tick-Tack-Toe, choose how many rounds you want to play: "
-)
+    try:
+        num_of_matches = int(num_of_matches)
+    except ValueError:
+        print("Not a number! Quitting game")
+        quit()
 
-try:
-    num_of_matches = int(num_of_matches)
+    return num_of_matches
 
-except ValueError:
-    print("Not a number! Quitting game")
-    quit()
 
-# Players entering their name and player objects getting instantiated
+# Function returning a list of player names
+def get_player_names():
+    player_names_list = []
+    input_name_1 = input("First player, enter your name: ")
+    player_names_list.append(input_name_1)
+    input_name_2 = input("Second player, enter your name: ")
+    player_names_list.append(input_name_2)
+    return player_names_list
 
-input_name1 = input("First player, enter your name: ")
-player_1 = Player(input_name1, "x")
 
-input_name2 = input("Second player, enter your name: ")
-player_2 = Player(input_name2, "o")
+# Getting players input for number of matches
+num_of_matches = get_num_matches()
+# Getting players input for player names
+player_names = get_player_names()
 
-new_board = Gameboard(num_of_matches)
-print(new_board)
+# Instantiating player classes
+player1 = Player(player_names[0], "x")
+player2 = Player(player_names[1], "o")
 
-# https://jayeshkawli.ghost.io/tic-tac-toe/
-new_board.draw_board()
 
-player_1.prompt_move()
-new_board.draw_board()
+game = Gameboard()
 
-player_2.prompt_move()
-new_board.draw_board()
+print(game)
+
+for number in range(0, 9):
+    game.draw_board()
+    player1.prompt_move()
+    game.draw_board()
+    player2.prompt_move()
+
+    if player1.determine_winning() is True:
+        player1.add_score()
+        break
+    elif player2.determine_winning() is True:
+        player2.add_score()
+        break
+
+
+print(player1.player_score)
+print(player2.player_score)
